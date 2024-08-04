4ba1b05572cdf9b6fe7b48d84e6db0bac266cea4
abhinav4545 <abhinavsingh333333@gmail.com>
2024-08-04 10:26:43 +0530

    add register controller and bug fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv" 
 import connectDB from "./db/index.js";
-import { log } from "console";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
